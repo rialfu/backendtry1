21da29d8919871081d8da1ac17cc1e980fd9ff12
rialfu <remasertu@gmail.com>
2022-09-28 14:30:35 +0700

    adding add

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -18,22 +18,52 @@ export default function Home() {
           setError(error);
         }
     }
-    if (error) {
-        return <div>Failed to load {error.toString()}</div>
-    }
-    //tidak ke
-    if (!data) {
-        return <div>Loading...</div>
-    }
-    if (!data.data){
-        return <div>Data kosong</div>
-    }
     return (
         <div>
-            {data.data.map((item, index) => (
-                <p key={index}>{item}</p>
-            ))}
-            {/* <p>data: {data.data}</p> */}
+            <Input onSuccess={getData} />
+            {error && <div>Failed to load {error.toString()}</div>}
+            {!data ? 
+                <div>Loading...</div> : 
+                ((data?.data ?? []).length === 0 && <p>data kosong</p>)
+            }
+            {data?.data ? data.data.map((item, index) => (
+        <p key={index}>{item}</p>
+      )) :
+        <p>data kosong</p>
+      }
+    
         </div>
     )
 }
+function Input({onSuccess}) {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const body = {
+          text: formData.get("data")
+        }
+    
+        try {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/send`, {
+                method: 'POST',
+                body: JSON.stringify(body)
+            });
+            const data = await res.json();
+            setData(data.message);
+            onSuccess();
+        }
+        catch (error) {
+            setError(error);
+        }
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+            <input name="data" type="text" />
+            <button >Submit</button>
+            </form>
+        </div>
+    )
+  }
